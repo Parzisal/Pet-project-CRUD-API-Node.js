1ab308c025973ff0ace1b5b6db0bfa033cb0cb3c
Andrei Zhuravskij <zhuravskij.andrei@gmail.com>
2021-02-23 22:29:56 +0300

    connected body-parser & cors & morgan

--- app/app.js
+++ app/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
 
+const app = express();
 const authRoutes = require('./routes/auth.routes')
+const bookRoutes = require('./routes/books.routes')
+
+app.use(morgan('dev'));
+app.use(cors());
+
+// to decode req.body
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/book', bookRoutes);
 
 module.exports = app;
--- app/routes/books.routes.js
+++ app/routes/books.routes.js
@@ -6,7 +6,7 @@ const controller = require('./../controllers/books.controllers')
 router.get('/', controller.getAll);
 router.get('/:id', controller.getById);
 router.post('/', controller.create);
-router.post('/:id', controller.updateById);
+router.patch('/:id', controller.updateById);
 router.delete('/:id', controller.removeById);
 
 module.exports = router;
